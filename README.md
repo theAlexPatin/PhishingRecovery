b89a8a3adfb1c2104cccf9d90d2158966f0e14f0
Alexander Patin <alex@squarelink.com>
2019-06-23 13:21:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Help I've been phished!
+
+A python script to save your ass after you've been phished. It will navigate to the phishing site and repeatedly populate their page with fake username and passwords.
+
+Ideally, they can't delineate between your real account and these fake accounts and won't get into your shit before you CHANGE YOUR PASSWORDS.
+
+## Dependencies
+
+- Python 3
+- Chromedriver
+
+## Installation
+
+```
+$ git clone https://github.com/theAlexPatin/PhishingRecovery.git
+$ cd PhishingRecovery
+$ pip install -r requirements.txt
+```
+
+## Usage
+
+- Change `URL` to the phishing url
+- Change `EMAIL_INPUT` to the name attribute of the email input field
+- Change `PASSWORD_INPUT` to the name attribute of the password field
+- Then run `$ python3 main.py`
+
+## Acknowledgements
+
+I'd like to thank the dummy who got phished and had me bail him or her out.
---
+++ main.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup as bs
+import random
+from faker import Faker
+fake = Faker()
+
+URL = 'https://www.rafola.appdealer.top/'
+EMAIL_INPUT = 'mail'
+PASSWORD_INPUT = 'password'
+
+def get_fake_email():
+	return fake.simple_profile()['mail']
+
+
+def _get_fake_phrase():
+	phrase = fake.words(3, None, True)
+	if not random.randint(0,3):
+		for i in range(len(phrase)):
+			phrase[i] = phrase[i].capitalize()
+	phrase = ''.join(str(e) for e in phrase)
+	if random.randint(0,1):
+		phrase = phrase + str(random.randint(0,100))
+	if random.randint(0,1):
+		phrase = phrase + '!'
+	return phrase
+
+def _get_fake_alphanum():
+	length = random.randint(8,16)
+	digits = random.randint(0,1)
+	upper_case = random.randint(0,1)
+	lower_case = random.randint(0,1)
+	special_chars = random.randint(0,1)
+	return fake.password(length, special_chars, digits, upper_case, lower_case)
+
+def get_fake_password():
+	if random.randint(0,5):
+		return _get_fake_phrase()
+	else:
+		return _get_fake_alphanum()
+
+def navigate(url):
+	driver = webdriver.Chrome()
+	driver.get(URL)
+	username = driver.find_element_by_name(EMAIL_INPUT)
+	username.send_keys(get_fake_email())
+	password = driver.find_element_by_name(PASSWORD_INPUT)
+	password.send_keys(get_fake_password())
+	password.send_keys(Keys.RETURN)
+
+
+def main():
+	while(True):
+		navigate()
+
+if __name__ == '__main__':
+	main()
